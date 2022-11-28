d9fdec1a8b99c42e54a0fdd751bfc5ad75622cbd
Elijahm19 <47839751+Elijahm19@users.noreply.github.com>
2022-11-28 12:19:12 -0500

    Update Queries.sql

--- queries/Queries.sql
+++ queries/Queries.sql
@@ -5,8 +5,12 @@
 
 
 -- Query 3
-
-
+SELECT p.policeid,p.lastname,count(c.crimeid)
+From police p, investigate i, crimecase c
+Where p.policeid = i.policeid AND i.crimeid = c.crimeid
+AND p.rank =  'Senior Police Officer'
+Group by p.policeid, p.lastname
+Order By count(c.crimeid) DESC;
 -- Query 4
 
 
